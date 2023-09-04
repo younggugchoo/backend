37c9719eebd27bee2179acadfbc8eb5ca8a93945
englandchoo <englandchoo@gmail.com>
2023-09-04 11:06:55 +0900

    asdf

--- src/main/java/org/choo/gallery/backend/controller/ItemController.java
+++ src/main/java/org/choo/gallery/backend/controller/ItemController.java
@@ -3,11 +3,11 @@ package org.choo.gallery.backend.controller;
 import org.choo.gallery.backend.entity.Item;
 import org.choo.gallery.backend.repostory.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @RestController
@@ -19,9 +19,8 @@ public class ItemController {
     public List<Item> getItems(){
 
         //aaasdfsdafas
-        List<Item> items = itemRepository.findAll();
-
-        return items;
+        return itemRepository.findAll();
+        //return items;
 
     }
 }
